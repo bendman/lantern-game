18c7aa61042a3b4f31006d0d9c55db09fcf5bc4a
bendman <duncan.ben@gmail.com>
2017-07-17 16:22:39 -0700

    player control of character

---
+++ Assets/Player/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	private float maxSpeed = 3f;
+
+	private bool userMoving = true;
+
+	private void Awake()
+	{
+		SwipeManager.OnSwipe += OnSwipe;
+	}
+
+	private void Update()
+	{
+		if (userMoving)
+		{
+			transform.Translate(Vector3.forward * maxSpeed * Time.deltaTime);
+		}
+	}
+
+	private void OnSwipe(SwipeManager.SwipeDirection direction, Vector2 delta)
+	{
+		// Ensure they swipe far enough, not just tap
+		if (Mathf.Abs(delta.x) <= 50f) { return; }
+		else if (direction == SwipeManager.SwipeDirection.Left)
+		{
+			TurnPlayer(Vector2.left);
+		}
+		else if (direction == SwipeManager.SwipeDirection.Right)
+		{
+			TurnPlayer(Vector2.right);
+		}
+	}
+
+	private void TurnPlayer(Vector2 direction)
+	{
+		float angle = (direction == Vector2.left) ? -90f : 90f;
+		transform.Rotate(0, angle, 0, Space.World);
+	}
+}
---
+++ Assets/Player/SwipeManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeManager : MonoBehaviour
+{
+	public enum SwipeDirection { Left, Right }
+
+	public delegate void Swipe(SwipeDirection direction, Vector2 delta);
+	public static event Swipe OnSwipe;
+
+	private Vector2 initialTouchPosition;
+	private SwipeManager instance;
+
+	private void Awake()
+	{
+		// Handle persistant instance
+		if (instance != null) { Destroy(gameObject); }
+		else
+		{
+			instance = this;
+			GameObject.DontDestroyOnLoad(gameObject);
+		}
+	}
+
+	private void Update()
+	{
+		HandleDowns();
+		HandleUps();
+	}
+
+	private void HandleDowns()
+	{
+		// Handle touch start
+		if (Input.touchSupported && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+		{
+			initialTouchPosition = Input.GetTouch(0).position;
+		}
+		// Handle mouse down
+		else if (Input.GetMouseButtonDown(0)) { initialTouchPosition = Input.mousePosition; }
+	}
+
+	private void HandleUps()
+	{
+		Vector2 finalTouchPosition;
+
+		// Handle touch end
+		if (Input.touchSupported && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+		{
+			finalTouchPosition = Input.GetTouch(0).position;
+		}
+		// Handle mouse up
+		else if (Input.GetMouseButtonUp(0)) { finalTouchPosition = Input.mousePosition; }
+		// No touch end or mouse up, so exit
+		else { return; }
+
+		Vector2 touchDelta = finalTouchPosition - initialTouchPosition;
+
+		SwipeDirection direction = touchDelta.x < 0 ? SwipeDirection.Left : SwipeDirection.Right;
+
+		// Handle OnSwipe events
+		if (OnSwipe != null) { OnSwipe(direction, touchDelta); }
+	}
+
+
+}
